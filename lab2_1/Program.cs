aafa27741fe513083a619d7a9237c04065345d94
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-03-07 21:21:35 +0200

    Add files via upload

---
+++ lab2_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace lab2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number from 1 to 7:");
+            int number = int.Parse(Console.ReadLine());
+
+            string colorName;
+            switch (number)
+            {
+                case 1:
+                    colorName = "red";
+                    break;
+                case 2:
+                    colorName = "orange";
+                    break;
+                case 3:
+                    colorName = "yellow";
+                    break;
+                case 4:
+                    colorName = "green";
+                    break;
+                case 5:
+                    colorName = "cyan";
+                    break;
+                case 6:
+                    colorName = "blue";
+                    break;
+                case 7:
+                    colorName = "violet";
+                    break;
+                default:
+                    Console.WriteLine("Invalid number.");
+                    return;
+            }
+
+            Console.WriteLine($"The color with serial number {number} is {colorName}.");
+        }
+    }
+}
