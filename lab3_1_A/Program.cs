28edb4c60d8472cb4602e9980863e6d4fef446d7
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-03-13 14:22:26 +0200

    Add files via upload

---
+++ lab3_1_A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab3_1_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text: ");
+            string text = Console.ReadLine();
+            int count = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsUpper(text[i]))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Count big words: {0}", count);
+            Console.ReadKey();
+        }
+    }
+}
