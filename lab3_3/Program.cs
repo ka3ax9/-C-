889cf3874ba93879f39910c3e9e8b30f852e9ba5
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-03-13 14:24:17 +0200

    Add files via upload

---
+++ lab3_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace lab3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set the full path to the input file
+            string inputPath = @"C:\Users\zenka\Documents\My_input_file\input.txt";
+
+            // Read input from file
+            string input = File.ReadAllText(inputPath);
+
+            // Remove all numbers from text
+            string output = Regex.Replace(input, @"\d+", "");
+
+            // Set the full path to the output file
+            string outputPath = @"C:\Users\zenka\Documents\My_input_file\output.txt";
+
+            // Write output to file
+            File.WriteAllText(outputPath, output);
+        }
+    }
+}
