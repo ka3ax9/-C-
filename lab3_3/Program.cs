ba4e9dc870dda1f3c48929e3883dacce4532950f
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-03-23 12:26:16 +0200

    Update Program.cs

--- lab3_3/Program.cs
+++ lab3_3/Program.cs
@@ -8,19 +8,19 @@ namespace lab3_3
     {
         static void Main(string[] args)
         {
-            // Set the full path to the input file
+         
             string inputPath = @"C:\Users\zenka\Documents\My_input_file\input.txt";
 
-            // Read input from file
+            
             string input = File.ReadAllText(inputPath);
 
-            // Remove all numbers from text
+            
             string output = Regex.Replace(input, @"\d+", "");
 
-            // Set the full path to the output file
+            
             string outputPath = @"C:\Users\zenka\Documents\My_input_file\output.txt";
 
-            // Write output to file
+           
             File.WriteAllText(outputPath, output);
         }
     }
