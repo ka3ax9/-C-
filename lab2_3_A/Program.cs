2787ad7319e40d2193942dc9b7be76817f5528f7
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-04-03 17:35:27 +0300

    Update Program.cs

--- lab2_3_A/Program.cs
+++ lab2_3_A/Program.cs
@@ -3,47 +3,91 @@
 namespace lab2_3_A
 {
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Get input for array size
+        int n = 0;
+        while (n <= 0)
         {
-            int n;
-            Console.Write("Enter the size of the array: ");
-            while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            Console.Write("Enter array size: ");
+            if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
             {
-                Console.Write("Invalid input. Please enter a positive integer: ");
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
             }
+        }
 
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++)
+        // Fill array with input from user
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            int num = 0;
+            while (true)
             {
-                Console.Write("Enter the value for element {0}: ", i);
-                while (!int.TryParse(Console.ReadLine(), out arr[i]))
+                Console.Write("Enter element {0}: ", i+1);
+                if (int.TryParse(Console.ReadLine(), out num))
                 {
-                    Console.Write("Invalid input. Please enter an integer: ");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer.");
                 }
             }
+            arr[i] = num;
+        }
 
-            int product = 1, sum = 0, lastPositiveIndex = -1;
-            for (int i = 0; i < n; i++)
+        // Calculate sum of elements before last positive element
+        int sum = 0;
+        int lastPositiveIndex = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > 0)
             {
-                if (arr[i] > 0)
-                {
-                    product *= arr[i];
-                    lastPositiveIndex = i;
-                }
+                lastPositiveIndex = i;
+            }
+        }
+
+        if (lastPositiveIndex < 0)
+        {
+            Console.WriteLine("No positive elements found.");
+        }
+        else if (lastPositiveIndex == 0)
+        {
+            Console.WriteLine("The first element is positive. The sum before the last positive element is zero.");
+        }
+        else
+        {
+            for (int i = 0; i < lastPositiveIndex; i++)
+            {
                 sum += arr[i];
             }
-
-            if (lastPositiveIndex == -1)
-            {
-                Console.WriteLine("There are no positive elements in the array.");
-            }
-            else
-            {
-                Console.WriteLine("Product of positive elements: " + product);
-                Console.WriteLine("Sum of elements up to the last positive element: " + sum);
-            }
-
+            Console.WriteLine("Sum of elements before last positive element: " + sum);
         }
+
+        // Calculate product of positive elements
+        int product = 1;
+        bool positiveFound = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > 0)
+            {
+                positiveFound = true;
+                product *= arr[i];
+            }
+        }
+
+        if (!positiveFound)
+        {
+            Console.WriteLine("No positive elements found. Product is 0.");
+        }
+        else
+        {
+            Console.WriteLine("Product of positive elements: " + product);
+        }
+
+        Console.ReadLine();
+    }
+    
     }
 }
