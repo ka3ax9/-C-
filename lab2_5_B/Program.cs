39c0e5cb0f2c508413ab8bff0285faa777e4fad4
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-04-03 17:33:57 +0300

    Update Program.cs

--- lab2_5_B/Program.cs
+++ lab2_5_B/Program.cs
@@ -1,60 +1,82 @@
-﻿using System;
+using System;
 
-namespace lab2_5_B
+namespace lab2_3_В
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.Write("Enter number of rows: ");
-            int n = int.Parse(Console.ReadLine());
-            Console.Write("Enter number of columns: ");
-            int m = int.Parse(Console.ReadLine());
+            // Get input for array size
+            int n = 0;
+            while (n <= 0)
+            {
+                Console.Write("Enter array size: ");
+                if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+                {
+                    Console.WriteLine("Invalid input. Please enter a positive integer.");
+                }
+            }
 
-           
-            int[,] matrix = new int[n, m];
-            Random rnd = new Random();
+            // Fill array with random values
+            int[] arr = new int[n];
+            Random rand = new Random();
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < m; j++)
-                {
-                    matrix[i, j] = rnd.Next(-100, 101);
-                }
+                arr[i] = rand.Next(-100, 101);
+                Console.Write(arr[i] + " ");
             }
+            Console.WriteLine();
 
-            
-            Console.WriteLine("Original matrix:");
-            PrintMatrix(matrix);
-
-            
-            for (int i = 0; i < n / 2; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    int temp = matrix[i, j];
-                    matrix[i, j] = matrix[n - i - 1, j];
-                    matrix[n - i - 1, j] = temp;
-                }
-            }
-
-           
-            Console.WriteLine("Modified matrix:");
-            PrintMatrix(matrix);
-        }
-
-        static void PrintMatrix(int[,] matrix)
-        {
-            int n = matrix.GetLength(0);
-            int m = matrix.GetLength(1);
+            // Calculate sum of elements before last positive element
+            int sum = 0;
+            int lastPositiveIndex = -1;
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < m; j++)
+                if (arr[i] > 0)
                 {
-                    Console.Write("{0,4}", matrix[i, j]);
+                    lastPositiveIndex = i;
                 }
-                Console.WriteLine();
             }
+
+            if (lastPositiveIndex < 0)
+            {
+                Console.WriteLine("No positive elements found.");
+            }
+            else if (lastPositiveIndex == 0)
+            {
+                Console.WriteLine("The first element is positive. The sum before the last positive element is zero.");
+            }
+            else
+            {
+                for (int i = 0; i < lastPositiveIndex; i++)
+                {
+                    sum += arr[i];
+                }
+                Console.WriteLine("Sum of elements before last positive element: " + sum);
+            }
+
+            // Calculate product of positive elements
+            int product = 1;
+            bool positiveFound = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positiveFound = true;
+                    product *= arr[i];
+                }
+            }
+
+            if (!positiveFound)
+            {
+                Console.WriteLine("No positive elements found. Product is 0.");
+            }
+            else
+            {
+                Console.WriteLine("Product of positive elements: " + product);
+            }
+
+            Console.ReadLine();
         }
     }
 }
