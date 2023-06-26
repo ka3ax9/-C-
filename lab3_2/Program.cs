b26e4ca7105d3fe72c70f5e6e785b03f465af239
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-06-26 22:22:13 +0300

    Update Program.cs

--- lab3_2/Program.cs
+++ lab3_2/Program.cs
@@ -1,47 +1,45 @@
-﻿using System;
+using System;
 using System.IO;
 
-namespace lab3_2
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Введення кількості елементів файлу
+        Console.Write("Введіть кількість елементів файлу: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Введення елементів файлу з клавіатури та запис у файл
+        Console.WriteLine("Введіть елементи файлу:");
+        using (StreamWriter writer = new StreamWriter("file.txt"))
         {
-            // Prompt user for filename and open file for writing
-            Console.Write("Enter filename: ");
-            string filename = Console.ReadLine();
-            StreamWriter writer = new StreamWriter(filename);
-
-            // Prompt user for number of elements to write to file and write them to file
-            Console.Write("Enter number of elements: ");
-            int numElements = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter real numbers:");
-            for (int i = 0; i < numElements; i++)
+            for (int i = 0; i < n; i++)
             {
-                double num = double.Parse(Console.ReadLine());
-                writer.WriteLine(num);
+                double element = double.Parse(Console.ReadLine());
+                writer.WriteLine(element);
             }
-            writer.Close();
+        }
 
-            // Prompt user for value to compare against
-            Console.Write("Enter value to compare against: ");
-            double n = double.Parse(Console.ReadLine());
+        // Введення числа n
+        Console.Write("Введіть число n: ");
+        double nValue = double.Parse(Console.ReadLine());
 
-            // Open file for reading and count number of elements greater than n
-            StreamReader reader = new StreamReader(filename);
-            int count = 0;
-            while (!reader.EndOfStream)
+        // Підрахунок кількості елементів файлу, більших за n
+        int count = 0;
+        using (StreamReader reader = new StreamReader("file.txt"))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
             {
-                double num = double.Parse(reader.ReadLine());
-                if (num > n)
+                double element = double.Parse(line);
+                if (element > nValue)
                 {
                     count++;
                 }
             }
-            reader.Close();
-
-            // Display result to user
-            Console.WriteLine($"Number of elements greater than {n}: {count}");
         }
+
+        // Виведення результату
+        Console.WriteLine("Кількість елементів файлу, більших за {0}: {1}", nValue, count);
     }
 }
