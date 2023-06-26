3a2413bae6cb5913168a6aefa4836aeddfa3b8d8
Yevhenii Kosolapov <94376914+ka3ax9@users.noreply.github.com>
2023-06-26 22:01:58 +0300

    Update Program.cs

--- lab4_2_TCPP/Program.cs
+++ lab4_2_TCPP/Program.cs
@@ -1,327 +1,279 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
-namespace HousingCommunalServices
+namespace CityDatabase
 {
+    class City
+    {
+        private string name;
+        private string address;
+        private string chiefLastName;
+        private int numberOfSubbuildings;
+        private string district;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string ChiefLastName
+        {
+            get { return chiefLastName; }
+            set { chiefLastName = value; }
+        }
+
+        public int NumberOfSubbuildings
+        {
+            get { return numberOfSubbuildings; }
+            set { numberOfSubbuildings = value; }
+        }
+
+        public string District
+        {
+            get { return district; }
+            set { district = value; }
+        }
+
+        public City()
+        {
+        }
+
+        public City(string name, string address, string chiefLastName, int numberOfSubbuildings, string district)
+        {
+            Name = name;
+            Address = address;
+            ChiefLastName = chiefLastName;
+            NumberOfSubbuildings = numberOfSubbuildings;
+            District = district;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nAddress: {Address}\nChief's Last Name: {ChiefLastName}\nNumber of Subbuildings: {NumberOfSubbuildings}\nDistrict: {District}\n";
+        }
+    }
+
+    class CityDatabase
+    {
+        private List<City> cities;
+        private string filePath;
+
+        public CityDatabase(string filePath)
+        {
+            this.filePath = filePath;
+            cities = new List<City>();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(';');
+                    City city = new City(data[0], data[1], data[2], int.Parse(data[3]), data[4]);
+                    cities.Add(city);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Database file not found. Creating a new database.");
+                File.Create(filePath).Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred while loading the database: {e.Message}");
+            }
+        }
+
+        private void SaveData()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (City city in cities)
+                    {
+                        string line = $"{city.Name};{city.Address};{city.ChiefLastName};{city.NumberOfSubbuildings};{city.District}";
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred while saving the database: {e.Message}");
+            }
+        }
+
+        public void AddCity(City city)
+        {
+            cities.Add(city);
+            SaveData();
+            Console.WriteLine("City added successfully.");
+        }
+
+        public void EditCity(int index, City updatedCity)
+        {
+            if (index >= 0 && index < cities.Count)
+            {
+                cities[index] = updatedCity;
+                SaveData();
+                Console.WriteLine("City updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid index. City not found.");
+            }
+        }
+
+        public void DeleteCity(int index)
+        {
+            if (index >= 0 && index < cities.Count)
+            {
+                cities.RemoveAt(index);
+                SaveData();
+                Console.WriteLine("City deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid index. City not found.");
+            }
+        }
+
+        public void DisplayCities()
+        {
+            if (cities.Count == 0)
+            {
+                Console.WriteLine("No cities found in the database.");
+            }
+            else
+            {
+                foreach (City city in cities)
+                {
+                    Console.WriteLine(city);
+                }
+            }
+        }
+
+        public void SearchByDistrict(string district)
+        {
+            List<City> matchedCities = cities.FindAll(city => city.District.Equals(district, StringComparison.OrdinalIgnoreCase));
+
+            if (matchedCities.Count == 0)
+            {
+                Console.WriteLine("No cities found in the specified district.");
+            }
+            else
+            {
+                foreach (City city in matchedCities)
+                {
+                    Console.WriteLine(city);
+                }
+            }
+        }
+
+        public void SortByName()
+        {
+            cities.Sort((x, y) => x.Name.CompareTo(y.Name));
+            Console.WriteLine("Cities sorted by name.");
+        }
+    }
+
     class Program
     {
-        static List<CityService> services;
-        static string filePath = "database.txt";
-
         static void Main(string[] args)
         {
-            services = LoadDataFromFile(filePath);
+            string filePath = "database.txt";
+            CityDatabase cityDatabase = new CityDatabase(filePath);
 
-            bool exit = false;
-            while (!exit)
+            while (true)
             {
-                Console.WriteLine("Menu:");
-                Console.WriteLine("1. Add record");
-                Console.WriteLine("2. Edit record");
-                Console.WriteLine("3. Delete record");
-                Console.WriteLine("4. Display all records");
-                Console.WriteLine("5. Search by city district");
-                Console.WriteLine("6. Sort by name");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("City Database");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("1. Add City");
+                Console.WriteLine("2. Edit City");
+                Console.WriteLine("3. Delete City");
+                Console.WriteLine("4. Display Cities");
+                Console.WriteLine("5. Search by District");
+                Console.WriteLine("6. Sort by Name");
                 Console.WriteLine("7. Exit");
-
+                Console.WriteLine("------------------------------");
                 Console.Write("Enter your choice: ");
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "1":
-                        AddRecord();
+                        Console.WriteLine("Enter city details:");
+                        Console.Write("Name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Address: ");
+                        string address = Console.ReadLine();
+                        Console.Write("Chief's Last Name: ");
+                        string chiefLastName = Console.ReadLine();
+                        Console.Write("Number of Subbuildings: ");
+                        int numberOfSubbuildings = int.Parse(Console.ReadLine());
+                        Console.Write("District: ");
+                        string district = Console.ReadLine();
+                        City newCity = new City(name, address, chiefLastName, numberOfSubbuildings, district);
+                        cityDatabase.AddCity(newCity);
                         break;
                     case "2":
-                        EditRecord();
+                        Console.WriteLine("Enter the index of the city to edit: ");
+                        int editIndex = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter updated city details:");
+                        Console.Write("Name: ");
+                        string updatedName = Console.ReadLine();
+                        Console.Write("Address: ");
+                        string updatedAddress = Console.ReadLine();
+                        Console.Write("Chief's Last Name: ");
+                        string updatedChiefLastName = Console.ReadLine();
+                        Console.Write("Number of Subbuildings: ");
+                        int updatedNumberOfSubbuildings = int.Parse(Console.ReadLine());
+                        Console.Write("District: ");
+                        string updatedDistrict = Console.ReadLine();
+                        City updatedCity = new City(updatedName, updatedAddress, updatedChiefLastName, updatedNumberOfSubbuildings, updatedDistrict);
+                        cityDatabase.EditCity(editIndex, updatedCity);
                         break;
                     case "3":
-                        DeleteRecord();
+                        Console.WriteLine("Enter the index of the city to delete: ");
+                        int deleteIndex = int.Parse(Console.ReadLine());
+                        cityDatabase.DeleteCity(deleteIndex);
                         break;
                     case "4":
-                        DisplayRecords();
+                        cityDatabase.DisplayCities();
                         break;
                     case "5":
-                        SearchByCityDistrict();
+                        Console.Write("Enter the district to search: ");
+                        string searchDistrict = Console.ReadLine();
+                        cityDatabase.SearchByDistrict(searchDistrict);
                         break;
                     case "6":
-                        SortByName();
+                        cityDatabase.SortByName();
                         break;
                     case "7":
-                        exit = true;
+                        Environment.Exit(0);
                         break;
                     default:
                         Console.WriteLine("Invalid choice. Please try again.");
                         break;
                 }
 
-                Console.WriteLine();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
             }
-
-            SaveDataToFile(filePath, services);
-        }
-
-        static void AddRecord()
-        {
-            Console.WriteLine("Add a new record:");
-            Console.Write("Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Address: ");
-            string address = Console.ReadLine();
-            Console.Write("Head: ");
-            string head = Console.ReadLine();
-            Console.Write("Number of reporting buildings: ");
-            int numBuildings = int.Parse(Console.ReadLine());
-            Console.Write("City district: ");
-            string district = Console.ReadLine();
-
-            CityService newService = new CityService(name, address, head, numBuildings, district);
-            services.Add(newService);
-
-            Console.WriteLine("Record added successfully.");
-        }
-
-        static void EditRecord()
-        {
-            Console.WriteLine("Edit a record:");
-            Console.Write("Enter the name of the service to edit: ");
-            string name = Console.ReadLine();
-
-            CityService service = services.FirstOrDefault(s => s.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-            if (service == null)
-            {
-                Console.WriteLine("Record not found.");
-                return;
-            }
-
-            Console.WriteLine("Enter new data for the record (leave empty for no change):");
-            Console.Write("Name: ");
-            string newName = Console.ReadLine();
-            Console.Write("Address: ");
-            string newAddress = Console.ReadLine();
-            Console.Write("Head: ");
-            string newHead = Console.ReadLine();
-            Console.Write("Number of reporting buildings: ");
-            string numBuildingsInput = Console.ReadLine();
-            Console.Write("City district: ");
-            string newDistrict = Console.ReadLine();
-
-            if (!string.IsNullOrEmpty(newName))
-                service.Name = newName;
-
-            if (!string.IsNullOrEmpty(newAddress))
-                service.Address = newAddress;
-
-            if (!string.IsNullOrEmpty(newHead))
-                service.Head = newHead;
-
-            if (!string.IsNullOrEmpty(numBuildingsInput))
-            {
-                if (int.TryParse(numBuildingsInput, out int newNumBuildings))
-                    service.NumBuildings = newNumBuildings;
-                else
-                    Console.WriteLine("Invalid number of reporting buildings. The value was not changed.");
-            }
-
-            if (!string.IsNullOrEmpty(newDistrict))
-                service.District = newDistrict;
-
-            Console.WriteLine("Record edited successfully.");
-        }
-
-        static void DeleteRecord()
-        {
-            Console.WriteLine("Delete a record: ");
-            Console.Write("Enter the name of the service to delete: ");
-            string name = Console.ReadLine();
-            CityService service = services.FirstOrDefault(s => s.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-            if (service == null)
-            {
-                Console.WriteLine("Record not found.");
-                return;
-            }
-
-            services.Remove(service);
-            Console.WriteLine("Record deleted successfully.");
-        }
-
-        static void DisplayRecords()
-        {
-            if (services.Count == 0)
-            {
-                Console.WriteLine("No records found.");
-                return;
-            }
-
-            Console.WriteLine("Records:");
-            foreach (CityService service in services)
-            {
-                Console.WriteLine(service.ToString());
-            }
-        }
-
-        static void SearchByCityDistrict()
-        {
-            Console.WriteLine("Search by city district:");
-            Console.Write("Enter the city district to search for: ");
-            string district = Console.ReadLine();
-
-            List<CityService> foundServices = services.Where(s => s.District.Equals(district, StringComparison.OrdinalIgnoreCase)).ToList();
-            if (foundServices.Count == 0)
-            {
-                Console.WriteLine("No records found in the specified city district.");
-                return;
-            }
-
-            Console.WriteLine("Records found:");
-            foreach (CityService service in foundServices)
-            {
-                Console.WriteLine(service.ToString());
-            }
-        }
-
-        static void SortByName()
-        {
-            services.Sort((s1, s2) => string.Compare(s1.Name, s2.Name, StringComparison.OrdinalIgnoreCase));
-
-            Console.WriteLine("Records sorted by name.");
-        }
-
-        static List<CityService> LoadDataFromFile(string filePath)
-        {
-            List<CityService> loadedServices = new List<CityService>();
-
-            if (File.Exists(filePath))
-            {
-                try
-                {
-                    string[] lines = File.ReadAllLines(filePath);
-
-                    foreach (string line in lines)
-                    {
-                        string[] fields = line.Split(';');
-                        if (fields.Length == 5)
-                        {
-                            string name = fields[0];
-                            string address = fields[1];
-                            string head = fields[2];
-                            int numBuildings = int.Parse(fields[3]);
-                            string district = fields[4];
-
-                            CityService service = new CityService(name, address, head, numBuildings, district);
-                            loadedServices.Add(service);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Під час завантаження даних з файлу виникла помилка:");
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-            return loadedServices;
-        }
-
-        static void SaveDataToFile(string filePath, List<CityService> services)
-        {
-            try
-            {
-                List<string> lines = new List<string>();
-
-                foreach (CityService service in services)
-                {
-                    string line = $"{service.Name};{service.Address};{service.Head};{service.NumBuildings};{service.District}";
-                    lines.Add(line);
-                }
-
-                File.WriteAllLines(filePath, lines);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Під час збереження даних в файл виникла помилка:");
-                Console.WriteLine(ex.Message);
-            }
-        }
-    }
-
-    class CityService
-    {
-        private string name;
-        private string address;
-        private string head;
-        private int numBuildings;
-        private string district;
-
-        public string Name
-        {
-            get { return name; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    throw new ArgumentException("Name cannot be empty.");
-                name = value;
-            }
-        }
-
-        public string Address
-        {
-            get { return address; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    throw new ArgumentException("Address cannot be empty.");
-                address = value;
-            }
-        }
-
-        public string Head
-        {
-            get { return head; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    throw new ArgumentException("Head cannot be empty.");
-                head = value;
-            }
-        }
-
-        public int NumBuildings
-        {
-            get { return numBuildings; }
-            set
-            {
-                if (value < 0)
-                    throw new ArgumentException("Number of reporting buildings cannot be negative.");
-                numBuildings = value;
-            }
-        }
-
-        public string District
-        {
-            get { return district; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    throw new ArgumentException("District cannot be empty.");
-                district = value;
-            }
-        }
-
-        public CityService(string name, string address, string head, int numBuildings, string district)
-        {
-            Name = name;
-            Address = address;
-            Head = head;
-            NumBuildings = numBuildings;
-            District = district;
-        }
-
-        public override string ToString()
-        {
-            return $"Name: {Name}\nAddress: {Address}\nHead: {Head}\nNumber of reporting buildings: {NumBuildings}\nDistrict: {District}\n";
         }
     }
 }
